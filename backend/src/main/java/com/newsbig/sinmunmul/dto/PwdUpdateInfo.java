cc957fa2725ab092b2f326cc41d7853b0ad4e678
손수연 <sksy368@naver.com>
2022-03-17 17:55:03 +0900

    📝 Feat: 추가 비밀번호 수정
    /{user_seq}/updatePassword API 호출 시 기존 비밀번호를 확인하고 유효성 검사 후 새 비밀번호로 업데이트
    해준다.
    
    Ref: #57

---
+++ backend/src/main/java/com/newsbig/sinmunmul/dto/PwdUpdateInfo.java
@@ -0,0 +1,16 @@
+package com.newsbig.sinmunmul.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("PwdUpdatePutRequest")
+public class PwdUpdateInfo {
+	@ApiModelProperty(name="변경 전 비밀번호", example="beforePwd!@#")
+	String userPwd;
+	@ApiModelProperty(name="변경 후 비밀번호", example="afterPwd!@#")
+	String newUserPwd;
+}
