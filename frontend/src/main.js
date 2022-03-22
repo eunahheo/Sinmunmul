ee98dbe403226e44bee7a26734f531f5ea5c32ad
김하영 <hayou94@gmail.com>
2022-03-22 11:00:53 +0900

    📝 Feat: 랜딩페이지 INIT

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import bootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="landing">
+        <img alt="신문물 logo" src="../assets/shin_logo.png" style="width: 10%">
+        <h1>신문물</h1>
+        <h2 style="height:50px"></h2>
+        <div>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <img src="../assets/shin_logo.png" alt="" style="width:20%">
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+            <h3>신문물은 신문사를 위한 신문물 서비스입니다.</h3>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
