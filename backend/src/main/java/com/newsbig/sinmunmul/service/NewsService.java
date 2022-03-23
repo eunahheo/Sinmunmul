197a7a6570ad63b6b42526560eef9dab6a7657e0
sdi1358_ <sdi1358_@naver.com>
2022-03-23 16:55:27 +0900

    📝 Feat: 추가 오늘의 뉴스 현황
    오늘의 뉴스 현황 API
    전체, 정치, 경제, 사회, 생활/문화, 세계, 과학 별 기사 개수와 전체 대비 퍼센트를 반환한다.
    
    Resolves: #111

--- backend/src/main/java/com/newsbig/sinmunmul/service/NewsService.java
+++ backend/src/main/java/com/newsbig/sinmunmul/service/NewsService.java
@@ -1,9 +1,12 @@
 package com.newsbig.sinmunmul.service;
 
+import java.util.List;
 import java.util.Map;
+
+import org.json.simple.JSONObject;
 
 public interface NewsService {
 	public void scrap(int newsSeq, int userSeq);
 	
-	public Map<String, Map<String, String>> todayNews();
+	public JSONObject todayNews();
 }
