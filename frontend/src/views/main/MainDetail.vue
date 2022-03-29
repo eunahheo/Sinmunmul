e45d89dcf2b7e671b256e873d3f8158ec13b9254
배용한 <byh5112@naver.com>
2022-03-29 21:35:46 +0900

    📝 Feat: 상세 페이지 페이징처리

--- frontend/src/views/main/MainDetail.vue
+++ frontend/src/views/main/MainDetail.vue
@@ -22,25 +22,90 @@
 
     <div class="plan span--2 long--2">
       <h3>키워드 언급량 추이 그래프</h3>
-      <line-chart :data="lineData" ></line-chart>
+      <!-- <line-chart :data="lineData" ></line-chart> -->
     </div>
   </main>
   
-<ul class="nav nav-tabs m-3" >
+<ul class="nav nav-tabs m-4" >
   <li class="nav-item container row" style="float: none; margin:100 auto;">
    <h3 class="">관련 뉴스 목록</h3>
-   
-   <br>
-   <hr><hr>
-  
-   
   </li>
  </ul>
+
+
+<div class="card m-2 " style="max-width: 1040px; max-height : 400px" v-for="news in searchedData" :key="news.news_seq">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img v-bind:src="news.news_photo" class="card-img-top image"  @error="replaceDefault">
+      <!-- <img v-bind:src="news.news.newsPhoto" class="img-fluid rounded-start"  @error="replaceDefault"> -->
+    </div>
+    <div class="col-md-7">
+      <div class="card-body">
+        <h5 class="card-title">{{news.news_Title}} </h5>
+        <!-- <p class="card-text">{{news.news_desc}} </p> -->
+        <!-- <p class="card-text"><small class="text-muted"> 작은 글씨</small></p> -->
+      </div>
+    </div>
+    <div class="col-md-1">
+      <!-- <button @click="detail">자세히 보기 </button> -->
+      <!-- <a :href="detail" class="btn btn-primary" style="display:block; width:100%; height:100%; vertical-align: middle;">자세히 보기</a> -->
+
+      <button @click="detail" class="btn btn-info" 
+      style="display:block; width:100%; height:100%; vertical-align: middle;"
+      v-bind:value="news.news_seq"
+      >자세히 보기</button>
+    </div>
+
+  </div>
+ 
+</div>
+
+
+
+
+
+  <nav aria-label="..." class="d-flex justify-content-center mb-4 ">
+      <ul class="pagination d-flex justify-content-between">
+        <li v-if="start" class="page-item">
+          <a class="page-link" href="#" @click="thisPage(1)">«</a>
+        </li>
+        <li v-else class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">«</a>
+        </li>
+
+        <li v-if="pre" li class="page-item">
+          <a class="page-link" href="#" @click="thisPage(nowPage-5)">‹</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">‹</a>
+        </li>
+        <li
+         v-for="pageitem in pageNumbers"
+          v-bind:id="'p'+pageitem"
+          v-bind:class="{' active': pageitem == nowPage}"
+          :key="pageitem"
+         class="page-item"><a class="page-link"  href="#" @click="thisPage(pageitem)">{{pageitem}}</a></li>
+        <li v-if="next" class="page-item">
+          <a class="page-link" href="#" @click="thisPage(nowPage+5)">›</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">›</a>
+        </li>
+
+        <li v-if="end" class="page-item">
+          <a class="page-link" href="#" @click="thisPage(totalPage)">»</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">»</a>
+        </li>
+      </ul>
+    </nav>
 </div>
 
 </div>
 </template>
 <script>
+import img from '@/assets/default.png'
 import axios from 'axios'
 // const LOCAL_HOST = 'http://localhost:3030/api'
 const SERVER_HOST = 'https://j6a406.p.ssafy.io/api'
@@ -49,7 +114,18 @@ export default {
   data() {
     return {
       searchWord : null,
-      searchedData : []
+      searchedData : [],
+      pageNumbers: [1, 2, 3, 4, 5],
+      page: 1, // 기본 1페이지
+      totalPage: '',
+      totalCount: '',
+      nowPage: 1,
+      startPage: '',
+      endPage: '',
+      pre: false,
+      next: false,
+      start: false,
+      end: false
     }
   },
   created() {
@@ -64,17 +140,17 @@ export default {
         axios.get(`${SERVER_HOST}/news/keyword`, {
           params: {
             keyword : this.searchWord,
-            page : 1,
-            size : 3
+            page : this.nowPage,
+            size : 5
           }
         })
         .then((res) =>{
          
-          // console.log(res.data);
-          this.searchedData = res.data;
-          // console.log(this.searchedData);
-          // console.log("=========================");
-          console.log(this.searchedData.data);
+          console.log(res.data);
+
+          this.searchedData = res.data.data;
+          const totalElements = res.data.data[0].totalElements;
+          this.pagination(totalElements);
 
           
         }).catch((err) => {
@@ -82,7 +158,61 @@ export default {
             console.log(err);
           })
       }
-    }
+    },
+
+    pagination: function (total) {
+      this.totalPage = parseInt(total / 6)
+      if ((total % 6) !== 0) {
+        this.totalPage = this.totalPage + 1
+      }
+      this.startPage = (parseInt((this.nowPage - 1) / 5)) * 5 + 1
+      this.endPage = this.startPage + 4
+
+      if (this.endPage > this.totalPage) {
+        this.endPage = this.totalPage
+      }
+      this.pre = false
+      this.start = false
+
+      if ((this.nowPage - 5) >= 1) {
+        this.pre = true
+      }
+      if (this.startPage > 5) {
+        this.start = true
+      }
+      this.next = false
+      this.end = false
+
+      if (this.endPage < this.totalPage) {
+        this.next = true
+        this.end = true
+      }
+
+      const s = this.startPage
+      const e = this.endPage
+      for (let i = s; i <= e; i++) {
+        this.pageNumbers[i - s] = i
+       
+      }
+    },
+    thisPage: function (num) {
+      if (num > this.totalPage) {
+        num = this.totalPage
+      }
+
+      this.nowPage = num
+      this.search();
+    },
+
+    replaceDefault: function (e) {
+      e.target.src = img
+    },
+    detail () {
+      var value = this.value;
+      console.log('모달창 띄우기 뉴스 시퀀스 : '+value);
+      
+    },
+
   }
   
 }
