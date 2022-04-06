67da4bc195660d6f73d3672c26cdf1b6266caa50
yminsang96 <yminsang96@gmail.com>
2022-04-07 00:10:51 +0900

    🐛 Fix: "수정 추천 기사 API"
    유저 시퀀스를 파라미터로 전송하면 해당 유저의 관심사에 맞게 긍정/부정 추천기사를 반환
    
    Ref: #151

--- backend/src/main/java/com/newsbig/sinmunmul/service/NewsRecommendServiceImpl.java
+++ backend/src/main/java/com/newsbig/sinmunmul/service/NewsRecommendServiceImpl.java
@@ -1,12 +1,21 @@
 package com.newsbig.sinmunmul.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.StringTokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.newsbig.sinmunmul.dto.CodeDto;
+import com.newsbig.sinmunmul.dto.InterestDto;
 import com.newsbig.sinmunmul.entity.News;
+import com.newsbig.sinmunmul.repository.InterestRepositorySupport;
 import com.newsbig.sinmunmul.repository.NewsRecommendRepositorySupport;
+import com.newsbig.sinmunmul.repository.NewsTopicRepository;
 
 @Service
 public class NewsRecommendServiceImpl implements NewsRecommendService{
@@ -14,10 +23,51 @@ public class NewsRecommendServiceImpl implements NewsRecommendService{
 	@Autowired
 	NewsRecommendRepositorySupport newsrRepositorySupport;
 	
-	@Override
-	public Map<String, News> recommentArticle(int code, String keyword) {
-		Map<String, News> result = newsrRepositorySupport.searchByCodeAndKeyword(code, keyword);
+	@Autowired
+	InterestRepositorySupport interestRepositorySupport;
+	
+	@Autowired
+	NewsTopicRepository newsTopicRepository;
+	
+	public class Keyword {
+		int code;
+		String word;
 		
+		public Keyword(int code, String word) {
+			super();
+			this.code = code;
+			this.word = word;
+		}
+	}
+	
+	@Override
+	public Map<String, News> recommentArticle(int userSeq) {
+		
+		// 사용자 관심사 코드들 불러오기
+		InterestDto interestDto = interestRepositorySupport.searchInterest(userSeq);
+		List<CodeDto> list = interestDto.getYesInterest();
+		
+		// 소분류에 해당하는 키워드들 불러오기
+		List<Keyword> keywords = new ArrayList<>();
+		StringTokenizer st = null;
+		
+		for(CodeDto codeDto : list) {
+			String keyword = newsTopicRepository.findBydelYnAndCode("n", codeDto.getCode()).getKeyword();
+			st = new StringTokenizer(keyword,",");
+			
+			while(st.hasMoreTokens()) {
+				keywords.add(new Keyword(codeDto.getCode(), st.nextToken()));
+			}
+		}
+		
+		// 키워드 중 랜덤으로 1개 선택
+		Random random = new Random();
+		int idx = random.nextInt(list.size());
+		
+		Keyword keyword = keywords.get(idx);
+		System.out.println("코드 : " +  keyword.code + ", 단어 : " + keyword.word);
+		Map<String, News> result = new HashMap<>();
+		result = newsrRepositorySupport.searchByCodeAndKeyword(keyword.code, keyword.word);
 		return result;
 	}
 
