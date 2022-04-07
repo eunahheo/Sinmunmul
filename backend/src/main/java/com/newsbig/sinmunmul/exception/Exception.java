aea889f87bdfaf667c32a1d2c25c430c7e5f8b88
yminsang96 <yminsang96@gmail.com>
2022-04-07 15:25:13 +0900

    🐛 Fix: "수정 추천 기사 API"
    추천 기사 API 업데이트

--- backend/src/main/java/com/newsbig/sinmunmul/exception/Exception.java
+++
@@ -1,5 +0,0 @@
-package com.newsbig.sinmunmul.exception;
-
-public class Exception {
-
-}
--- backend/src/main/java/com/newsbig/sinmunmul/service/NewsRecommendServiceImpl.java
+++ backend/src/main/java/com/newsbig/sinmunmul/service/NewsRecommendServiceImpl.java
@@ -1,6 +1,8 @@
 package com.newsbig.sinmunmul.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +14,9 @@ import org.springframework.stereotype.Service;
 
 import com.newsbig.sinmunmul.dto.CodeDto;
 import com.newsbig.sinmunmul.dto.InterestDto;
+import com.newsbig.sinmunmul.dto.RecomNewsDto;
 import com.newsbig.sinmunmul.entity.News;
+import com.newsbig.sinmunmul.exception.NotRecommendException;
 import com.newsbig.sinmunmul.repository.InterestRepositorySupport;
 import com.newsbig.sinmunmul.repository.NewsRecommendRepositorySupport;
 import com.newsbig.sinmunmul.repository.NewsTopicRepository;
@@ -43,6 +47,7 @@ public class NewsRecommendServiceImpl implements NewsRecommendService{
 	@Override
 	public Map<String, Object> recommentArticle(int userSeq) {
 		
+		Map<String, Object> result = new HashMap<>();
 		// 사용자 관심사 코드들 불러오기
 		InterestDto interestDto = interestRepositorySupport.searchInterest(userSeq);
 		List<CodeDto> list = interestDto.getYesInterest();
@@ -53,22 +58,47 @@ public class NewsRecommendServiceImpl implements NewsRecommendService{
 		
 		for(CodeDto codeDto : list) {
 			String keyword = newsTopicRepository.findBydelYnAndCode("n", codeDto.getCode()).getKeyword();
-			System.out.println("코드리스트 : " + codeDto.getCode() + ",  키워드 : "+keyword);
 			st = new StringTokenizer(keyword,",");
 			
 			while(st.hasMoreTokens()) {
-				keywords.add(new Keyword(codeDto.getCode(), st.nextToken()));
+				keywords.add(new Keyword(codeDto.getCodeGroup(), st.nextToken()));
 			}
 		}
 		
-		// 키워드 중 랜덤으로 1개 선택
-		Random random = new Random();
-		int idx = random.nextInt(keywords.size());
 		
-		Keyword keyword = keywords.get(idx);
-		System.out.println("코드 : " +  keyword.code + ", 단어 : " + keyword.word);
-		Map<String, Object> result = new HashMap<>();
-		result = newsrRepositorySupport.searchByCodeAndKeyword(keyword.code, keyword.word);
+		
+		Map<String, Object> obj = new HashMap<>();
+		List<News> newsList = new ArrayList<>();
+		List<RecomNewsDto> miniNewsList = new ArrayList<>();		
+		Keyword keyword = null;
+		Random random = new Random();
+		int cnt = 0;
+		
+		while(newsList.isEmpty() || newsList.size()<2) {
+			// 키워드 중 랜덤으로 1개 선택
+			int idx = random.nextInt(keywords.size());		
+			keyword = keywords.get(idx);
+			System.out.println("인덱스 : " + idx +"코드 : "+ keyword.code +"키워드 : " + keyword.word);
+			
+			obj = newsrRepositorySupport.searchByCodeGroupAndKeyword(keyword.code, keyword.word);			
+			newsList = (ArrayList<News>) obj.get("News");
+			if(cnt++>50) {
+				throw new NotRecommendException();
+			}
+		}
+		
+		for(News news : newsList) {
+			miniNewsList.add(new RecomNewsDto(news.getNewsSeq(),Math.abs(news.getNewsPositive())-Math.abs(news.getNewsNegative()),
+					news.getNewsPhoto(), news.getNewsTitle(), news.getNewsSummary()));
+		}
+		
+		Collections.sort(miniNewsList);
+		
+		result.put("codeGroup", keyword.code);
+		result.put("keyword", keyword.word);
+		result.put("positive", miniNewsList.get(0));
+		result.put("negative", miniNewsList.get(miniNewsList.size()-1));
+		
 		return result;
 	}
 
